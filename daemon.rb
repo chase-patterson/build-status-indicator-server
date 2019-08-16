f5aec4349ea8b3791f27fc8237888121b418ce9b
Chase Patterson <chase.patterson@sourcetoad.com>
2019-08-15 23:38:19 -0400

    Add some MQTT routines

--- daemon.rb
+++ daemon.rb
@@ -1,5 +1,6 @@
 require 'singleton'
 require 'json'
+require 'mqtt'
 
 require_relative 'pipeline.rb'
 require_relative 'exceptions.rb'
@@ -7,15 +8,69 @@ require_relative 'exceptions.rb'
 module BuildStatusIndicator
   class Daemon
     include Singleton
+
+    attr_reader :pipelines, :indicators, :controllers
   
     def initialize
+      @mqtt_broker_addr = '127.0.0.1'
+
       @pipelines = []
       @indicators = []
       @controllers = []
       @msg_callbacks = []
+
+      self.start_sub_zigbee2mqtt_log
+
+      self.get_devices
     end
-  
-    attr_reader :pipelines, :indicators, :controllers
+
+    def get_devices
+      sub_client = nil
+
+      sub_thread = Thread.new do
+        sub_client = MQTT::Client.connect(@mqtt_broker_addr)
+        topic, message = sub_client.get 'zigbee2mqtt/bridge/config/devices'
+        sub_client.disconnect
+        puts message
+      end
+
+      Thread.new do
+        # Wait until our subscriber is really listening
+        retries = 0
+        while sub_client.nil? || !sub_client.connected?
+          if retries > 50
+            sub_thread.terminate
+            sub_client.disconnect if !sub_client.nil?
+            raise BSIException, "Timed-out waiting for subscriber"
+          end
+          sleep 0.1
+          retries += 1
+        end
+
+        MQTT::Client.connect(@mqtt_broker_addr) do |c|
+          c.publish 'zigbee2mqtt/bridge/config/devices/get', ""
+        end
+      end
+    end
+
+    def start_sub_zigbee2mqtt_log
+      if @zigbee2mqtt_log_thread.nil?
+        @sub_zigbee2mqtt_log_thread = Thread.new do
+          MQTT::Client.connect(@mqtt_broker_addr) do |c|
+            c.get 'zigbee2mqtt/bridge/log' do |topic, message|
+              puts "#{topic}: #{message}"
+            end
+          end
+        end
+      end
+    end
+
+    def stop_sub_zigbee2mqtt_log
+      unless @zigbee2mqtt_log_thread.nil?
+        @zigbee2mqtt_log_thread.terminate
+        @zigbee2mqtt_log_thread = nil
+      end
+    end
   
     def add_pipeline props
       pipeline = Pipeline.new(props)
--- rack_app.ru
+++ rack_app.ru
@@ -3,6 +3,9 @@ require 'rack/cors'
 require_relative 'websocket_middleware.rb'
 require_relative 'api_app.rb'
 
+require_relative 'daemon.rb'
+BuildStatusIndicator::Daemon.instance # Call once to initialize singleton instance
+
 app = Rack::Builder.new do
   use Rack::Cors do
     allow do
